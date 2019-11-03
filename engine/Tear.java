97bbd7a94b5304bef0fa94f0b0ce061542be2dcd
paul10175 <41805033+paul10175@users.noreply.github.com>
2019-11-03 17:00:53 -0500

    Now Tear starts at the head

--- engine/Tear.java
+++ engine/Tear.java
@@ -6,23 +6,40 @@ import java.awt.image.BufferedImage;
 public class Tear extends GameObject{
 
 	protected Direction lastDirection;
-	protected BufferedImage bg;
+	protected boolean useRightEye;
 	
-	public Tear(int x, int y, Direction lastDirection, BufferedImage img) {
+	public Tear(int x, int y, Direction lastDirection, boolean whichEye) {
+		this.useRightEye = whichEye;
+		this.lastDirection = lastDirection;
 		at = new AffineTransform();
 		at.setToIdentity();
-		at.setToScale(.2, .2);
 		at.translate(x, y);
-		this.lastDirection = lastDirection;
-		this.bg = img;
+		setXYPosition();
+	}
+	
+	
+	//Four different cases we have to deal with when placing the tear in the correct position.
+	//North, east, south and west. In each case we have to check which eye we need to use. 
+	public void setXYPosition() {
+		if (lastDirection == Direction.NORTH) {
+			if (useRightEye == true)
+				at.translate(35, 25);
+			else 
+				at.translate(5, 25);
+		} else if (lastDirection == Direction.SOUTH) {
+			if (useRightEye == true)
+				at.translate(5, 25);
+			else 
+				at.translate(35, 25);
+		} else if (lastDirection == Direction.EAST) {
+			at.translate(30,20);
+		} else 
+			at.translate(10, 20);
+		
+		at.scale(.25, .25);
 	}
 	
 	public Direction getLastDirection() {
 		return lastDirection;
 	}
-	
-	public BufferedImage getImage() {
-		return bg;
-	}
-
 }
