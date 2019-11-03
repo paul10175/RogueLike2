d8c8bc79f7e8d006bed023a7e3ce4bfb422728c8
paul10175 <41805033+paul10175@users.noreply.github.com>
2019-11-03 16:58:02 -0500

    Can now shoot tears

--- engine/HeadComponent.java
+++ engine/HeadComponent.java
@@ -1,12 +1,17 @@
 package engine;
 
 import java.awt.Graphics2D;
+import java.util.ArrayList;
 
 public class HeadComponent extends Component {
+	protected Character character;
+	protected boolean rightEyeUsed = true;
+	
 	
 	public HeadComponent(Character parent) {
 		super(parent);
 //		this.direction = d;
+		this.character = parent;
 		this.setPriority(Priority.HEAD);
 	}
 	
@@ -18,13 +23,26 @@ public class HeadComponent extends Component {
 	public void logic() {
 		// AffineTransform here
 //		System.out.println(InputHandler.W_PRESSED);
-		if (InputHandler.W_PRESSED)
+		if (InputHandler.W_PRESSED) {
 			parent.at.translate(0,-2);
-		if (InputHandler.A_PRESSED)
+			character.setDirection(Direction.NORTH);
+		}if (InputHandler.A_PRESSED) {
 			parent.at.translate(-2, 0);
-		if (InputHandler.S_PRESSED)
+			character.setDirection(Direction.WEST);
+		}if (InputHandler.S_PRESSED) {
 			parent.at.translate(0, 2);
-		if (InputHandler.D_PRESSED)
+			character.setDirection(Direction.SOUTH);
+		}if (InputHandler.D_PRESSED) {
 			parent.at.translate(2, 0);
+			character.setDirection(Direction.EAST);
+		}if (InputHandler.SPACE_PRESSED) {
+			Character character = (Character)parent;
+			Tear tear = new Tear((int)parent.at.getTranslateX(), (int)parent.at.getTranslateY(), character.lastDirection, rightEyeUsed);
+			tear.addComponent(new TearMovement(tear));
+			EngineCore.elements.add(tear);
+			rightEyeUsed = !rightEyeUsed;
+			InputHandler.SPACE_PRESSED = false;
+		}
+			
 	}
 }
