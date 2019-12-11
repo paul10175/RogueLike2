2bcc9f227e042ef63dfe48365c46f4c42b1cb6bc
1j4k7 <jasonkuo147@gmail.com>
2019-12-10 21:15:56 -0500

    graph progress

--- engine/GameObjectGraph.java
+++ engine/GameObjectGraph.java
@@ -1,6 +1,7 @@
 package engine;
 
 import java.awt.geom.AffineTransform;
+import java.awt.geom.NoninvertibleTransformException;
 import java.util.Stack;
 
 public class GameObjectGraph extends GameObject {
@@ -16,6 +17,17 @@ public class GameObjectGraph extends GameObject {
 		Stack<GameObjectNode> nodeStack = new Stack<GameObjectNode>();
 		Stack<AffineTransform> transStack = new Stack<AffineTransform>();
 		// Invert the camera transform and push it into the stack first
+//		AffineTransform cameraTrans = camera.object.at;
+//		try {
+//			cameraTrans.invert();
+//		} catch (NoninvertibleTransformException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		nodeStack.push(camera);
+//		transStack.push(cameraTrans);
+		nodeStack.push(head);
+		transStack.push(head.object.at);
 		while (!nodeStack.isEmpty()) {
 			GameObjectNode currNode = nodeStack.pop();
 			AffineTransform currTrans = transStack.pop();
